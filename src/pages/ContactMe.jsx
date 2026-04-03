5ed290e0c46228bd87aeaf3082d1c03b7005976f
ritika bhangade <bhangaderitika80@gmail.com>
2026-04-03 14:38:36 +0530

    updated backend url

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -20,7 +20,7 @@ const ContactMe = () => {
     setStatus('sending');
 
     try {
-      const response = await fetch('https://portfolio-backend-s832.onrender.com', {
+      const response = await fetch('https://portfolio-backend-s832.onrender.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
